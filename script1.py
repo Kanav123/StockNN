657d455853b526be8a89a73aa3f14dc271f2c285
Kanav <kanav.vats@gmail.com>
2017-04-04 15:54:33 +0530

    adds LSTM prediction and a README

--- script1.py
+++ script1.py
@@ -6,39 +6,32 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
 
-data = pd.read_csv('tab.csv')
-data.info()
-data = data.drop('Date',1)
-data = data.drop('Adj Close',1)
+stock_data = pd.read_csv('tab.csv',delimiter= ',')
+close_prices = stock_data[['Close']].values[::-1]
 
-#normalize data in [0,1] range
-min_max_scaler = preprocessing.MinMaxScaler()
-normalized_data = min_max_scaler.fit_transform(data)
-rev_data = normalized_data[::-1]
-np.savetxt('stock_norm.csv',rev_data,delimiter=",")
-stock_data = np.genfromtxt('stock_norm.csv',delimiter= ',')
-new_data = []
-for i in range(len(stock_data) -1):
-    d = []
-    for elt in stock_data[i]:
-        d.append(elt)
-    d.append(stock_data[i+1][0])
-    d = np.array(d)
-    new_data.append(d)
+dataset,normalised_data = [],[]
+DS = SupervisedDataSet(50 ,1)
+for i in range(0,len(close_prices) - 50):
+  dataset.append(close_prices[i:i+51])
 
-DS = SupervisedDataSet(5 ,1)
-my_data = np.array(new_data)
-train_data = my_data[: int(.6*len(my_data))]
-test_data = my_data[ int(.6*len(my_data)): len(my_data)]
+#normalize in a window to reflect percentage change from the start
+for window in dataset:
+  normalised_window = [((float(p) / float(window[0])) - 1) for p in window]
+  normalised_data.append(normalised_window)
+
+my_data = np.array(normalised_data)
+train_data = my_data[:round( .6*len(my_data))]
+test_data = my_data[ round(.6*len(my_data)): len(my_data)]
 
 for elt in train_data:
-   DS.addSample(elt[:5],elt[5])
+   DS.addSample(elt[:50],elt[50])
 
 hidden_layers = [4,8,12]
+
 for layer in hidden_layers:
   FNN = buildNetwork(DS.indim, layer, DS.outdim, bias=True)
 
-  TRAINER = BackpropTrainer(FNN, dataset=DS, learningrate = 0.001, \
+  TRAINER = BackpropTrainer(FNN, dataset=DS, learningrate = 0.01, \
      momentum=0.1, verbose=True)
   print "No of nodes in hidden layers:" + str(layer)
   trnerr,valerr = TRAINER.trainUntilConvergence(maxEpochs = 10)
@@ -51,13 +44,14 @@ for layer in hidden_layers:
   plt.show()
   predicted = []
   for elt in test_data:
-     predicted.append(FNN.activate(elt[:5]))
+     predicted.append(FNN.activate(elt[:50]))
   fig = plt.figure()
   ax = fig.add_subplot(111)
   ax.set_title('Predicted vs Actual')
   ax.set_xlabel('days')
   ax.set_ylabel('value')
-  plt.plot([i for i in range(0,len(test_data))] , test_data[:,5])
+  plt.plot([i for i in range(0,len(test_data))] , test_data[:,50])
   plt.plot([i for i in range(0,len(test_data))] , predicted , 'r')
   plt.show()
+
 
